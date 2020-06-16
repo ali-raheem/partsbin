30c327ff3201a226a1fe94e286dbf925b544b1f4
Ali Raheem <ali.raheem@gmail.com>
2020-06-16 22:51:56 +0100

    Update UART_TX.v

--- UART/UART_TX.v
+++ UART/UART_TX.v
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Ali Raheem <ali.raheem@gmail.com>
+   Copyright 2020 Ali Raheem <ali.raheem@gmail.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
