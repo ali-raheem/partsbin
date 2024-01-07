4ce7236354641c36e01eedb7f5db3a637b8df548
Ali Raheem <ali.raheem@gmail.com>
2024-01-07 19:59:51 +0000

    Update piso_tb.v
    Needs adapting to the more recent version (more connections)

--- HC1645/piso_tb.v
+++ HC1645/piso_tb.v
@@ -13,6 +13,8 @@
    See the License for the specific language governing permissions and limitations under the License.
 */
 
+/// THIS IS OLD
+
 module piso_tb ();
    localparam WIDTH = 16;
    reg clk;
